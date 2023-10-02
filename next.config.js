44ecd3d0555de11b7c16a1be504eecbff3040e9f
PedrooMota <pedrohmota2005@gmail.com>
2023-10-02 17:05:13 -0300

    build: Novas dependências e configuração do Next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const { blackA, mauve, violet } = require('@radix-ui/colors');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-  }
+
+  output: "export",
+
+  images: {
+    unoptimized: true
+  },
+ 
+  };
   
   module.exports = nextConfig
