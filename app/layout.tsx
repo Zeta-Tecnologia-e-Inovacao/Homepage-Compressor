4d769ee8c51f91a2dd9b3d44734173a217bb165f
Pedro Henrique Mota <83295376+PedrooMota@users.noreply.github.com>
2023-10-23 15:52:02 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,12 @@ import { SessionProvider } from "next-auth/react"
 //   display: 'swap'
 // })
 
-const architects_daughter = Architects_Daughter({
-  subsets: ['latin'],
-  variable: '--font-architects-daughter',
-  weight: '400',
-  display: 'swap'
-})
+// const architects_daughter = Architects_Daughter({
+//   subsets: ['latin'],
+//   variable: '--font-architects-daughter',
+//   weight: '400',
+//   display: 'swap'
+// })
 
 export default function RootLayout({
   children,
@@ -42,11 +42,12 @@ export default function RootLayout({
     })
   })
 // {`${inter.variable}
+// ${architects_daughter.variable} font-inter
   return (
 
     <html lang="en">
       <SessionProvider session={session}>
-        <body className={`${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
+        <body className={`antialiased bg-gray-900 text-gray-200 tracking-tight`}>
           <Theme>
             <div className="flex flex-col min-h-screen overflow-hidden">
               <main className="grow">
